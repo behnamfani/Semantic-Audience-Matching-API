d287550b4578f1798b6b14854bf69d1868cfd632
Behnam Fanitabasi <behnam.fani18@gmail.com>
2024-03-24 02:14:05 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="1.0",
+    description="Semantic Matching API",
+    author="Behnam Fanitabasi",
+    license="MIT",
+)
